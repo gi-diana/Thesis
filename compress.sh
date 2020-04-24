69ebe7cec395e8ea799663f8e41d19cf7e3176b0
Luigi Di Rado <luigi.dirado@gmail.com>
2020-04-24 18:44:37 +0200

    compress function script e pdf-a

---
+++ compress.sh
@@ -0,0 +1,2 @@
+gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPrinted=false -dPDFSETTINGS=/prepress -dNOPAUSE -dQUIET -dBATCH -sOutputFile=Tesi_small.pdf Tesi.pdf
+gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPrinted=false -dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH -sOutputFile=Tesi_micro.pdf Tesi.pdf
